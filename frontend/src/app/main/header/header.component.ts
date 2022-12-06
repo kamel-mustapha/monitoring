ce4a2d522ba2732ba82aebb0b46f5a3425b6697d
kamel <musk96@hotmail.fr>
2022-12-06 23:02:04 +0100

    coded front

--- frontend/src/app/main/header/header.component.ts
+++ frontend/src/app/main/header/header.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SharedService } from 'src/app/services/shared.service';
+import { animations } from 'src/app/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
+  animations: animations,
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string = 'home';
   @Input() icon: string = 'fa-home';
-  constructor() {}
+  constructor(private shared: SharedService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.shared.shown_popups_subject.subscribe((value) => {
+      this.shown_popups = value;
+    });
+  }
+  shown_popups: any = {};
+  show_hide_element(elem: string) {
+    this.shared.show_hide_element(elem);
+    console.log(this.shown_popups);
+  }
 }
