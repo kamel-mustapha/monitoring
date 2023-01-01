e06ba462b92883d0fffd9ebcb4a811a5a6a0270c
kamel <musk96@hotmail.fr>
2023-01-01 14:07:41 +0100

    fixed popup frontend

--- frontend/src/app/popup/popup.component.ts
+++ frontend/src/app/popup/popup.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SharedService } from '../services/shared.service';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css']
+  styleUrls: ['./popup.component.css'],
 })
 export class PopupComponent implements OnInit {
-
-  constructor() { }
+  constructor(private shared: SharedService) {}
 
   ngOnInit(): void {
+    this.shared.shown_popups_subject.subscribe((value) => {
+      this.shown_popups = value;
+    });
   }
+  shown_popups: any = {};
 
+  show_creation_window() {
+    this.shared.show_hide_element('monitor_creation');
+  }
 }
