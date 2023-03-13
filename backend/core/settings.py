bbc7003a120cb69b7015505474b9f6906920cf12
musk96 <musk96@hotmail.fr>
2023-03-13 22:29:50 +0100

    added some responsive

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -63,24 +63,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / "db.sqlite3"
-#         }
-#     }
-# else:
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': CREDS.get("DB_NAME"),
-        'USER': CREDS.get("DB_USER"),
-        'PASSWORD': CREDS.get("DB_PASSWORD"),
-        'HOST': 'db',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3"
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': CREDS.get("DB_NAME"),
+#         'USER': CREDS.get("DB_USER"),
+#         'PASSWORD': CREDS.get("DB_PASSWORD"),
+#         'HOST': 'db',
+#         'PORT': '3306',
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
