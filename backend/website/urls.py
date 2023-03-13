8728b0e13bc9a516e454542b243d319d9fe96246
musk96 <musk96@hotmail.fr>
2023-03-13 21:42:52 +0100

    added plans in angular ursl

--- backend/website/urls.py
+++ backend/website/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
 
-ANGULAR_PATHS = ["dashboard", "pages", "profile"]
+ANGULAR_PATHS = ["dashboard", "pages", "profile", "plans"]
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
